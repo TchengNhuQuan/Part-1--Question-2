5a9de3a42aaab1c9afed58a76aa5a54f7a298c63
Lamtrinh22 <lamsum22@example.com>
2022-07-14 22:03:26 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react' 
+import ReactDOM from 'react-dom' 
+
+// Tạo component App
+function App() {
+    return ( <div>
+        <h1> Part 1 | Question 2 </h1> </div>
+    )
+}
+
+ReactDOM.render( < App / > , document.getElementById('root'))
